6bef062bef0293db5413789913e8321a1af5eb2e
Rafaelbrk75 <rafaelbrk75@gmail.com>
2026-03-22 19:47:44 -0300

    ProdutoRepository criado

---
+++ src/main/java/br/edu/unicesumar/trabalho/repository/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package br.edu.unicesumar.trabalho.repository;
+
+import br.edu.unicesumar.trabalho.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
