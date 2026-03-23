0bfe6738d35641d4b1560b103eab9fbb04d7503e
Rafaelbrk75 <rafaelbrk75@gmail.com>
2026-03-22 22:40:35 -0300

    Inciando atividade 4

---
+++ src/main/java/br/edu/unicesumar/trabalho/entity/Autor.java
@@ -0,0 +1,51 @@
+package br.edu.unicesumar.trabalho.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_autor")
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String nacionalidade;
+    private LocalDate dataNascimento;
+
+    public Autor() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
