e5b91cdc477cdbfb06ff27c4f00f74528c26c1fa
Rafaelbrk75 <rafaelbrk75@gmail.com>
2026-03-23 19:49:00 -0300

    Atividade 9 finalizada

---
+++ src/main/java/br/edu/unicesumar/trabalho/controller/ProjetoController.java
@@ -0,0 +1,47 @@
+package br.edu.unicesumar.trabalho.controller;
+
+import br.edu.unicesumar.trabalho.entity.Projeto;
+import br.edu.unicesumar.trabalho.service.ProjetoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/projeto")
+public class ProjetoController {
+
+    @Autowired
+    private ProjetoService projetoService;
+
+    @PostMapping
+    public ResponseEntity<Projeto> criarProjeto(@RequestBody Projeto projeto) {
+        Projeto request = projetoService.criarProjeto(projeto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(request.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Projeto>> listarProjetos() {
+        List<Projeto> request = projetoService.listarProjetos();
+        return ResponseEntity.ok().body(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Projeto> buscarProjetoPorId(Long id) {
+        Projeto request = projetoService.buscarProjetoPorId(id);
+        return ResponseEntity.ok().body(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProjetoPorId(Long id) {
+        projetoService.deletarProjetoPorId(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
